a019f8c9803dbf5574a176203f3655a359ee1243
steadfasterX <steadfasterX@gmail.com>
2021-03-11 15:06:31 +0100

    j5y17lte:vendorsetup: remove http ota
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,7 +7,6 @@ export EOS_DEVICE=j5y17lte
 export EOS_BRANCH_NAME="v1-q"
 export EOS_RELEASE_TYPE="UNOFFICIAL"
 #export EOS_CUSTOM_PACKAGES="MuPDF GmsCore GsfProxy FakeStore com.google.android.maps.jar Mail BlissLauncher BlissIconPack MozillaNlpBackend OpenWeatherMapWeatherProvider AccountManager MagicEarth OpenCamera eDrive Weather Notes Tasks NominatimNlpBackend DroidGuard OpenKeychain Message Browser BrowserWebView LibreOfficeViewer noEOSappstore"
-export EOS_OTA_CLEARTEXT=true
 export EOS_USE_CCACHE=true
 export EOS_CCACHE_DIR=/ssd/ccache/jenkins/android-10.0
 export EOS_CCACHE_SIZE=14G
