3da81bc634bd0d5622c0eee0a3ca88c75bf928ba
steadfasterX <steadfasterx@noreply.mail-is-hidden.local>
2022-09-05 17:27:44 +0200

    remove obsolete var definitions
    moved everything to Ansible/Semaphore

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,16 +3,16 @@ export ALLOW_MISSING_DEPENDENCIES=true
 
 ############# /e/ section ###############
 
-export EOS_DEVICE=j5y17lte
-export EOS_BRANCH_NAME="v1-q"
+#export EOS_DEVICE=j5y17lte
+#export EOS_BRANCH_NAME="v1-q"
 #export EOS_RELEASE_TYPE="CUSTOM"
 #export EOS_CUSTOM_PACKAGES="MuPDF GmsCore GsfProxy FakeStore com.google.android.maps.jar Mail BlissLauncher BlissIconPack MozillaNlpBackend OpenWeatherMapWeatherProvider AccountManager MagicEarth OpenCamera eDrive Weather Notes Tasks NominatimNlpBackend DroidGuard OpenKeychain Message Browser BrowserWebView LibreOfficeViewer noEOSappstore"
-export EOS_USE_CCACHE=true
-export EOS_CCACHE_DIR=/ssd/ccache/jenkins/android-10.0
-export EOS_CCACHE_SIZE=14G
-export EOS_TMP_DIR=/ssd/tmp/eos
-export EOS_ZIP_DIR=/ssd/tmp/eos/zips
-export EOS_CLEAN_ZIPDIR=false
+#export EOS_USE_CCACHE=true
+#export EOS_CCACHE_DIR=/ssd/ccache/jenkins/android-10.0
+#export EOS_CCACHE_SIZE=14G
+#export EOS_TMP_DIR=/ssd/tmp/eos
+#export EOS_ZIP_DIR=/ssd/tmp/eos/zips
+#export EOS_CLEAN_ZIPDIR=false
 #export EOS_CLEAN_BEFORE_BUILD=true
 
 ########### extendrom section ###########
